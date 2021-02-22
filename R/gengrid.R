ee181364a0b1147e5146515c639ab8a0d83c04fa
Ifeanyi Edochie <ifeanyi.edochie@gmail.com>
2021-02-22 11:55:52 -0500

    some updates to gengrid that add arguments for grid_size in kilometers, coordinate reference systems

--- R/gengrid.R
+++ R/gengrid.R
@@ -1,11 +1,25 @@
+#' Polygonize a raster and compute zonal statistics at polygon and full shapefile level
+#'
+#' @param dsn stands for data source name (see sf::st_read documentation for more information).
+#' @param layer layer name (see sf::st_read documentation for more information)
+#' @param stats zonal statistics to be estimated
+#' @param raster_tif raster file with tif extension
+#' @param grid_size the diagonal length of the polygon in km
+#' @param crs the coorindate reference system to be used
+#' @return list object aggregated zonal statistic, polygon data and summary statistics on polygon data
+#' @examples
+
 
 gengrid <- function(dsn = "data-raw",
                     layer = "gadm36_CMR_0",
                     stats = "sum",
-                    raster_tif = "cmr_ppp_2020_UNadj_constrained.tif"){
+                    raster_tif = "cmr_ppp_2020_UNadj_constrained.tif",
+                    grid_size = 1,
+                    crs = '+proj=longlat +datum=WGS84 +no_defs'
+                    ){
 
   ## below are the packages needed for the function to run
-  usepkgs <- c( "sf","raster","tidyr","dplyr","spex",
+  usepkgs <- c( "sf","raster","tidyr","dplyr","spex", "stars",
                 "exactextractr","tmap", "rgdal", "data.table")
 
   missing <- usepkgs[!(usepkgs %in% installed.packages()[,"Package"])]
@@ -18,29 +32,30 @@ gengrid <- function(dsn = "data-raw",
 
   invisible(sapply(usepkgs, library, character.only = TRUE))
 
-  shp <- st_read(dsn = dsn,
+  shp <- sf::st_read(dsn = dsn,
                  layer= layer)
-  pop <- raster(paste("data-raw", raster_tif, sep = "/"))
+  pop <- raster::raster(paste("data-raw", raster_tif, sep = "/"))
 
   #generate baseline raster
-  base_raster <- raster(xmn= -180, ymn= -90, xmx = 180, ymx = 90, resolution = 0.008333,
-                        crs = '+proj=longlat +datum=WGS84 +no_defs')
-  extent_shp <- extent(shp)
-  base_raster <- crop(base_raster, extent_shp)
+  resolution <- grid_size/111
+  base_raster <- raster::raster(xmn= -180, ymn= -90, xmx = 180, ymx = 90,
+                                resolution = resolution, crs = crs)
+  extent_shp <- raster::extent(shp)
+  base_raster <- raster::crop(base_raster, extent_shp)
   base_raster[is.na(base_raster)] <- 0
   base_raster <- raster::mask(base_raster, shp)
 
   #change raster to polygon
-  br_poly <- polygonize(base_raster)
+  br_poly <- spex::polygonize(base_raster)
   class(br_poly)
-  br_poly <- br_poly %>% rename(id = layer)
+  br_poly <- br_poly %>% dplyr::rename(id = layer)
   br_poly$id <- seq(1:dim(br_poly)[1])
 
   #now compute zonal statistics of population
-  zonal_stats <- exact_extract(pop, br_poly, stats) %>% as.data.table()
+  zonal_stats <- exactextractr::exact_extract(pop, br_poly, stats) %>% as.data.table()
   names(zonal_stats) <- stats
   br_poly <- cbind(br_poly,zonal_stats)
-  mymap <- tm_shape(br_poly) +
+  mymap <- tmap::tm_shape(br_poly) +
     tm_fill(stats,
             title = raster_tif,
             style="quantile",
@@ -48,7 +63,7 @@ gengrid <- function(dsn = "data-raw",
             palette="PuBu")  +
     tm_borders(col="black", lwd=0, alpha = 0)
 
-  br_dt <- as.data.table(br_poly[,stats])
+  br_dt <- data.table::as.data.table(br_poly[,stats])
 
 
   return(list(total_popsize = sum(br_dt[, stats, with=F]),
@@ -56,3 +71,41 @@ gengrid <- function(dsn = "data-raw",
               summary_stats = summary(br_dt)))
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
