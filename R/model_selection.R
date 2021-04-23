b1ee6b1d5129697ca0d066381bf78bed3d60bbab
Ifeanyi Edochie <ifeanyi.edochie@gmail.com>
2021-04-23 11:50:45 -0400

    created a new function for model selection, the function needs to be tested

--- R/model_selection.R
+++ R/model_selection.R
@@ -1,45 +1,46 @@
+#' Select Model for Small Area Estimation
 #'
+#' A function to select the set of geospatial variables to be used for a household level small area estimation model
 #'
-#' @param data Data source name
-#' @param outcomevar Independent variable
+#' @param dt object of class data.table or data.frame
+#' @param outcomevar character vector of size 1 naming the outcome variable
+#' @param dropvars a list of variables to be omitted by the selection algorithm, all other variables will be included
 #'
-#' @return summary of the lasso regression and a list of the coefficients of each regressor
+#' @return a list summarizing the lasso regression output as well as the set of coefficients
 #'
 #' @export
 #'
 #' @import data.table sf haven bestNormalize hdm dplyr tidyr glmnet BBmisc
 
 
+model_selection <- function(dt, outcomevar, dropvars) {
 
-model_selection(data,
-                outcomevar){
-
-  # Preparing data
-  Y_t <- orderNorm(data[ ,outcomevar])
+  # Preparing dt
+  Y_t <- orderNorm(dt[ ,outcomevar])
   Y_t <- Y_t$x.t
 
-  X <- data[ ,as.factor(prefecture),waterperm_2018julsep:length_unclassified]
+  X <- dt[ ,as.factor(prefecture),waterperm_2018julsep:length_unclassified]
   X <- X[ ,-c("id.y")]
   X[is.na(X)] <- 0
 
-  lengths <- X %>% dplyr::select(starts_with("length"))
-  densities <- X %>% dplyr:: select(starts_with("roaddensity"))
-  counts <- X %>% dplyr:: select(starts_with("count"))
-  points <- X %>% dplyr:: select(ends_with("pointcount"))
+  # lengths <- X %>% dplyr::select(starts_with("length"))
+  # densities <- X %>% dplyr:: select(starts_with("roaddensity"))
+  # counts <- X %>% dplyr:: select(starts_with("count"))
+  # points <- X %>% dplyr:: select(ends_with("pointcount"))
 
 
   X <- X[ ,waterperm_2018julsep:bld_totallength]
-  X <- data.table(X,lengths,counts,points)
+  X <- dt.table(X,lengths,counts,points)
 
 
   # Lasso regression
-  lasso.reg <-  rlasso(Y_t~ .,data=X,post=TRUE)
+  lasso.reg <-  rlasso(Y_t~ .,dt=X,post=TRUE)
   coefs <- lasso.reg$beta[lasso.reg$index==TRUE]
 
 
 
   return(list(regression_results = summary(lasso.reg),
-              coefficients = br_poly))
+              coefficients = coefs))
+
 
 }
-
