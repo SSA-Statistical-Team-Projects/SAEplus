d60d9fc368631b0800b17c7e97a7b24b10320551
Ifeanyi Edochie <ifeanyi.edochie@gmail.com>
2021-05-18 15:36:45 -0400

    Update emdi_gin.R
    push latest updates

--- data-raw/emdi_gin.R
+++ data-raw/emdi_gin.R
@@ -3,8 +3,15 @@
 gin_hhcensus.dt <- readRDS("data/gin_hhcensus.RDS")
 gin_hhsurvey.dt <- readRDS("data/gin_hhsurvey.RDS")
 
+load("data/gin_selectedvars.RDS")
+
+gin_model <- paste(selected.vars, collapse = " + ")
+gin_model <- as.formula(paste("pcexp", gin_model, sep = " ~ "))
+
 
 ###store the GINevnrionment and run ENDI in another script
 ginemdi_model <- emdi::ebp(fixed = gin_model, pop_data = gin_hhcensus.dt, pop_domains = "ADM3_CODE",
                            smp_data = gin_hhsurvey.dt, smp_domains = "ADM3_CODE", threshold = 0,
                            L = 100, transformation = "no", na.rm = TRUE)
+
+
