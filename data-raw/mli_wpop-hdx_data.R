2e47e0d18f269b65d6ede630e7171aee4ddcb1e1
rahim03 <rahimatoumemboup@gmail.com>
2021-05-26 11:59:29 +0000

    Worldpop and hdx data for MLI

--- data-raw/mli_wpop-hdx_data.R
+++ data-raw/mli_wpop-hdx_data.R
@@ -15,57 +15,57 @@ unzip("data-raw/MLI_buildings_v1_1.zip", exdir = "data")
 ##### Merging all the building data
 dt <- gengrid(dsn = "data",
               layer = "afr_mli_l04",
-              raster_tif = "MLI_buildings_v2_0_count.tif",
+              raster_tif = "MLI_buildings_v1_1_count.tif",
               grid_shp = T,
               featname = "bld_count",
               drop_Zero = F)
-tcd.bld.dt <- as.data.table(dt$polygon_dt)
+mli.bld.dt <- as.data.table(dt$polygon_dt)
 dt <- gengrid(dsn = "data",
               layer = "afr_mli_l04",
-              raster_tif = "MLI_buildings_v2_0_cv_area.tif",
+              raster_tif = "MLI_buildings_v1_1_cv_area.tif",
               stats = "mean",
               grid_shp = T,
               featname = "bld_cvarea",
               drop_Zero = F)
 add.dt <- as.data.table(dt$polygon_dt)
-tcd.bld.dt <- cbind(tcd.bld.dt, add.dt[,"bld_cvarea"])
+mli.bld.dt <- cbind(mli.bld.dt, add.dt[,"bld_cvarea"])
 dt <- gengrid(dsn = "data",
               layer = "afr_mli_l04",
-              raster_tif = "MLI_buildings_v2_0_cv_length.tif",
+              raster_tif = "MLI_buildings_v1_1_cv_length.tif",
               stats = "mean",
               grid_shp = T,
               featname = "bld_cvlength",
               drop_Zero = F)
 add.dt <- as.data.table(dt$polygon_dt)
-tcd.bld.dt <- cbind(tcd.bld.dt, add.dt[,"bld_cvlength"])
+mli.bld.dt <- cbind(mli.bld.dt, add.dt[,"bld_cvlength"])
 dt <- gengrid(dsn = "data",
               layer = "afr_mli_l04",
-              raster_tif = "MLI_buildings_v2_0_density.tif",
+              raster_tif = "MLI_buildings_v1_1_density.tif",
               stats = "mean",
               grid_shp = T,
               featname = "bld_density",
               drop_Zero = F)
 add.dt <- as.data.table(dt$polygon_dt)
-tcd.bld.dt <- cbind(tcd.bld.dt, add.dt[,"bld_density"])
+mli.bld.dt <- cbind(mli.bld.dt, add.dt[,"bld_density"])
 dt <- gengrid(dsn = "data",
               layer = "afr_mli_l04",
-              raster_tif = "MLI_buildings_v2_0_mean_area.tif",
+              raster_tif = "MLI_buildings_v1_1_mean_area.tif",
               stats = "mean",
               grid_shp = T,
               featname = "bld_meanarea",
               drop_Zero = F)
 add.dt <- as.data.table(dt$polygon_dt)
-tcd.bld.dt <- cbind(tcd.bld.dt, add.dt[,"bld_meanarea"])
+mli.bld.dt <- cbind(mli.bld.dt, add.dt[,"bld_meanarea"])
 dt <- gengrid(dsn = "data",
               layer = "afr_mli_l04",
-              raster_tif = "MLI_buildings_v2_0_total_length.tif",
+              raster_tif = "MLI_buildings_v1_1_total_length.tif",
               grid_shp = T,
               stats = "mean",
               featname = "bld_totallength",
               drop_Zero = F)
 add.dt <- as.data.table(dt$polygon_dt)
-tcd.bld.dt <- cbind(tcd.bld.dt, add.dt[,"bld_totallength"])
-saveRDS(tcd.bld.dt, "data/MLI_allbuilding.RDS")
+mli.bld.dt <- cbind(mli.bld.dt, add.dt[,"bld_totallength"])
+saveRDS(mli.bld.dt, "data/MLI_allbuilding.RDS")
 
 
 ##### Pulling in the Relative well index data
