40753ff0b0cbd29d51aa6e842ac9f4b84411f2e9
Ifeanyi Edochie <ifeanyi.edochie@gmail.com>
2021-06-07 15:55:06 -0400

    update on emdi run with lc data
    emdi model run with landcover data included

--- data-raw/emdi_gin.R
+++ data-raw/emdi_gin.R
@@ -5,7 +5,9 @@ library(emdi)
 gin_hhcensus.dt <- readRDS("data/gin_hhcensus.RDS")
 gin_hhsurvey.dt <- readRDS("data/gin_hhsurvey.RDS")
 
-load("data/gin_selectedvars.RDS")
+selected.vars <- readRDS("data/gin_selectedvars.RDS")
+selected.vars <- selected.vars[!grepl("count_secondary_link", selected.vars)]
+selected.vars <- selected.vars[!grepl("length_secondary_link", selected.vars)]
 
 gin_model <- paste(selected.vars, collapse = " + ")
 gin_model <- as.formula(paste("pcexp", gin_model, sep = " ~ "))
@@ -25,28 +27,38 @@ gin_model <- as.formula(paste("pcexp", gin_model, sep = " ~ "))
 #
 
 #### compute the value
-
-
-
-#################################################################################################################
-### Beyond this point will only be on a SERVER with multiple cores
-
-# ginemdi_model2 <- emdi_ebp2(fixed = gin_model, pop_data = as.data.frame(gin_hhcensus.dt), pop_domains = "ADM3_CODE",
-#                             smp_data = as.data.frame(gin_hhsurvey.dt), smp_domains = "ADM3_CODE", threshold = -0.4486192,
-#                             L = 100, transformation = "no", na.rm = TRUE, smp_weight = "hhweight",
-#                             pop_weight = "ind_estimate", cpus = 25, MSE = TRUE)
 #
-# saveRDS(ginemdi_model2, "data/ginemdi_model2")
+#
+#
+# #################################################################################################################
+# ### Beyond this point will only be on a SERVER with multiple cores
+#
+ginemdi_model2 <- emdi_ebp2(fixed = gin_model, pop_data = as.data.frame(gin_hhcensus.dt), pop_domains = "ADM3_CODE",
+                            smp_data = as.data.frame(gin_hhsurvey.dt), smp_domains = "ADM3_CODE", threshold = -0.4486192,
+                            L = 100, transformation = "no", na.rm = TRUE, smp_weight = "hhweight", B = 100,
+                            pop_weight = "ind_estimate", cpus = 15, MSE = TRUE)
 
-### lets try to run the write.excel file
+saveRDS(ginemdi_model2, "data/ginemdi_model2.RDS")
 
-ginemdi_model2 <- readRDS("data/ginemdi_model2")
+ginemdi_model2 <- readRDS("data/ginemdi_model2.RDS")
 
-emdi_writeexcel(ginemdi_model2,
-                file = "data-raw/gin_emdiresults.xlsx",
-                indicator = "all",
-                MSE = TRUE,
-                CV = TRUE)
+
+emdi_writeexcel(ginemdi_model2, file = "data/emdi_results.xlsx",
+                indicator = "all", MSE = TRUE, CV = TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
