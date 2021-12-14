45e02f135ff7adcee7f207ef1ecc9dd5f470a4d9
Ifeanyi Edochie <ifeanyi.edochie@gmail.com>
2021-12-14 17:30:46 -0500

    moz test work for daylan
    pushing some code for the moz_test and updates to the editted gengrid2 function.

--- R/gengrid2.R
+++ R/gengrid2.R
@@ -8,16 +8,19 @@
 #' when shp_dt is not specified.
 #' @param shp_layer character; the layer name for the shapefile. Must be specified with shp_dsn when shp_dt is not
 #' specified
-#' @param grid_size numeric of length 1; representing the desired size of the grid
+#' @param grid_size numeric of length 1; representing the desired size of the grid in meters
 #' @param sqr logical; if TRUE, a square grid is created. If FALSE, a hexagonal polygon grid is created
-#' @param pop_raster raster; full file path of a raster object
+#' @param pop_raster raster; an object of class 'raster'
 #' @param raster_path character; if pop_raster is not specified but raster is to read in from file. raster_path is
 #' the full name of the raster (including filepath)
 #' @param extract_name character of length 1; the name of the indicator to be extracted from the raster
 #' @param raster_function function to be applied in extracting raster into created grids
+#'
+#' @importFrom raster raster
+#' @importFrom units set_units
 
 
-gengrid2 <- function(shp_dt,
+gengrid2 <- function(shp_dt = NULL,
                      shp_dsn,
                      shp_layer,
                      grid_size,
@@ -27,10 +30,12 @@ gengrid2 <- function(shp_dt,
                      extract_name,
                      raster_function = "sum") {
 
+  sf_use_s2(FALSE) ##just to ensure we don't begin to have issues with duplicate vertices
+
   if(is.null(shp_dt) == TRUE) {
 
-    shp_dt <- sf::st_read(dsn = shp_dsn,
-                          layer = shp_layer)
+    shp_dt <- st_read(dsn = shp_dsn,
+                      layer = shp_layer)
 
   }
 
@@ -38,16 +43,16 @@ gengrid2 <- function(shp_dt,
   ## now we are ready to grid our district shapefile
   if (sqr == TRUE) {
 
-  grid_system <- sf::st_make_grid(x = shp_dt,
-                                  cellsize = c(grid_size, grid_size),
-                                  square = sqr) %>%
+  grid_system <- st_make_grid(x = shp_dt,
+                              cellsize = c(grid_size, grid_size),
+                              square = sqr) %>%
     sf::st_sf()
 
   } else if (sqr == FALSE) {
 
-  grid_system <- sf::st_make_grid(x = shp_dt,
-                                  cellsize = grid_size,
-                                  square = sqr) %>%
+  grid_system <- st_make_grid(x = shp_dt,
+                              cellsize = grid_size,
+                              square = sqr) %>%
     sf::st_sf()
 
 
@@ -56,29 +61,30 @@ gengrid2 <- function(shp_dt,
 
   ## the process creates squares with parts outside the GMB area so we should take the intersection
   ## of the shapefile with our newly created grid
-  grid_system <- sf::st_intersection(grid_system, shp_dt)
+  grid_system <- st_intersection(grid_system, shp_dt)
 
-  grid_system$poly_area <- sf::st_area(grid_system) ##compute area of each square
-  grid_system$poly_area <- units::set_units(grid_system$poly_area, "km^2")
+  grid_system$poly_area <- st_area(grid_system) ##compute area of each square
+  grid_system$poly_area <- set_units(grid_system$poly_area, "km^2")
 
   summary(grid_system$poly_area) ## summary statistics show that are mostly 0.04sqkm with few exceptions
 
   ##ensuring the geometry is validly created
   grid_system <- st_make_valid(grid_system)
 
+
+
   ##extract population raster into the data
   ### read in the raster file
   if(is.null(raster_path) == FALSE) {
-    pop_raster <- raster::raster(raster_path)
+    pop_raster <- raster(raster_path)
     }
 
-  pop_raster <- raster::projectRaster(from = pop_raster,
-                                      crs = raster::crs(shp_dt))
-
+  ### convert crs of shapefile to the crs of the raster
+  grid_system <- st_transform(x = grid_system, crs = pop_raster@crs)
   ### extract value into the grid system
-  zonal_stats <- exactextractr::exact_extract(x = pop_raster,
-                                              y = grid_system,
-                                              fun = raster_function) %>% data.table()
+  zonal_stats <- exact_extract(x = pop_raster,
+                               y = grid_system,
+                               fun = raster_function) %>% data.table()
 
   colnames(zonal_stats) <- extract_name
 
